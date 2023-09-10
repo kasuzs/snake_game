b79f3e316e773e8b817610609740a39f0f16dae0
Stepan <rentv537@gmail.com>
2023-09-10 18:00:57 +0300

    added snake, direction, main game logic

--- snake_game/snake_game.cpp
+++ snake_game/snake_game.cpp
@@ -1,22 +1,48 @@
 ﻿#include <iostream>
 #include <windows.h>
+#include <string>
 using namespace std;
 
-const int WIDTH = 25;
-const int HEIGHT = 15;
+void gotoxy(int x, int y) {						     //
+	COORD pos = { x, y };						     //
+	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE); // display clear
+	SetConsoleCursorPosition(output, pos);           // 
+}													 //
 
-void gotoxy(int x, int y) {
-	COORD pos = { x, y };
-	HANDLE output = GetStdHandle(STD_OUTPUT_HANDLE);
-	SetConsoleCursorPosition(output, pos);
-}
+const int HEIGHT = 30;
+const int WIDTH = 50;
 
 char snake = 'O';
 
-int main()
-{
-	char arr[HEIGHT][WIDTH];
+int snake_X = WIDTH / 2;
+int snake_Y = HEIGHT / 2;
 
+bool isRunning = true;
+
+void LoadingMenu()
+{
+	string text = "Loading";
+	for (int i = 0; i < 4; ++i)
+	{
+		cout << "\n\n\n\n\n\n\n\n\n\n\t\tOptimize the system\n\n";
+		cout << "\t\t     " << text << "\n\n\n\n\n\n\n\n\n\n";
+		Sleep(500);
+		gotoxy(0, 0);
+		text += ".";
+	}
+}
+
+void isEnd()
+{
+	if (snake_X == 0 || snake_Y == 0 || WIDTH - 1 == snake_X || HEIGHT - 1 == snake_Y)
+	{
+		isRunning = false;
+	}
+}
+
+
+void createArea(char arr[HEIGHT][WIDTH])
+{
 	for (int i = 0; i < HEIGHT; ++i)
 	{
 		for (int j = 0; j < WIDTH; ++j)
@@ -31,15 +57,119 @@ int main()
 			}
 		}
 	}
+}
 
+void printArea(char arr[HEIGHT][WIDTH])
+{
 	for (int i = 0; i < HEIGHT; ++i)
 	{
-		for (int j = 0; j < WIDTH; ++j) 
+		for (int j = 0; j < WIDTH; ++j)
 		{
 			cout << arr[i][j];
 		}
 		cout << endl;
 	}
+}
+
+
+
+
+int UP = 0;     // W
+int DOWN = 1;	// S
+int LEFT = 2;   // A
+int RIGHT = 3;  // D
+
+int direction = RIGHT;
+
+
+
+int main()
+{
+	LoadingMenu();
+
+	char arr[HEIGHT][WIDTH];
+
+	createArea(arr);
+
+	while (isRunning)
+	{
+		if (GetKeyState('W') & 0x8000)
+		{
+			if (direction != DOWN)
+			{
+				direction = UP;
+			}
+		}
+		if (GetKeyState('S') & 0x8000)
+		{
+			if (direction != UP)
+			{
+				direction = DOWN;
+			}
+		}
+		if (GetKeyState('A') & 0x8000)
+		{
+			if (direction != RIGHT)
+			{
+				direction = LEFT;
+			}
+		}
+		if (GetKeyState('D') & 0x8000)
+		{
+			if (direction != LEFT)
+			{
+				direction = RIGHT;
+			}
+		}
+
+		if (direction == UP)
+		{
+			arr[snake_Y][snake_X] = ' ';
+			snake_Y--;
+		}
+
+		if (direction == DOWN)
+		{
+			arr[snake_Y][snake_X] = ' ';
+			snake_Y++;
+		}
+
+		if (direction == LEFT)
+		{
+			arr[snake_Y][snake_X] = ' ';
+			snake_X--;
+		}
+
+		if (direction == RIGHT)
+		{
+			arr[snake_Y][snake_X] = ' ';
+			snake_X++;
+		}
+		
+
+		arr[snake_Y][snake_X] = snake;
+
+		printArea(arr);
+
+		cout << "X pos: " << snake_X << endl;
+		cout << "Y pos: " << snake_Y << endl;
+
+		
+
+		gotoxy(0, 0);
+
+		isEnd();
+		if (isRunning == false)
+		{
+			system("CLS");
+			system("color 2");
+			cout << "\n\n\n\n\n\n\n\n\n\n\t\t\tGame Over\n\n\n\n\n\n\n\n\n\n" << endl;
+			return 0;
+		}
+		
+	}
+	
+
 	
 	
 	return 0;
